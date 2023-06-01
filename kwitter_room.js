a9b4fc02658d8c0a2d45c82cab4e00c6f5660804
CODINGMASTER1269 <114401548+CODINGMASTER1269@users.noreply.github.com>
2023-06-01 17:04:16 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+  
+const firebaseConfig = {
+  apiKey: "AIzaSyDUVG_nLum9RXsLch8IZAKJD28Z71oE0UE",
+  authDomain: "kwitterpro-5ed3b.firebaseapp.com",
+  projectId: "kwitterpro-5ed3b",
+  storageBucket: "kwitterpro-5ed3b.appspot.com",
+  messagingSenderId: "1055439814303",
+  appId: "1:1055439814303:web:cd68384d78064da57d6caa",
+  measurementId: "G-N70TH31JHY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
